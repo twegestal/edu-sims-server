53406c5413920281a750d2d20041b27dbaf65280
twegestal <96296150+twegestal@users.noreply.github.com>
2024-02-20 15:48:57 +0100

    init server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import { createServer } from './server';
+import dotenv from 'dotenv';
+import { createServer } from './server.js';
 
+dotenv.config();
 const server = createServer();
+const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => {
-  console.log('server listening on port 3000');
+server.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
 });
--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ export const createServer = () => {
   const app = express();
 
   app.use(express.json());
+  app.get('/', async (_req, res) => {
+    res.status(200).json('hej');
+  })
 
   return app;
 }
