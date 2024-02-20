c265007a647a561ec228e1a915a2e904c7b87209
twegestal <96296150+twegestal@users.noreply.github.com>
2024-02-20 17:33:49 +0100

    Test extended route

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ export const createServer = () => {
   app.get('/', async (_req, res) => {
     res.status(200).json('hej');
   })
+  app.get('/health', async (_req, res) => {
+    res.status(200).json('Im healthy!');
+  })
 
   return app;
 }
