846013347da06067af99c32fc8e1c005d9daefd5
twegestal <96296150+twegestal@users.noreply.github.com>
2024-02-20 19:38:10 +0100

    added cors

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 export const createServer = () => {
   const app = express();
 
+  app.use(cors());
   app.use(express.json());
   app.get('/', async (_req, res) => {
     res.status(200).json('hej');
-  })
+  });
   app.get('/health', async (_req, res) => {
     res.status(200).json('Im healthy!');
-  })
+  });
 
   return app;
 }
