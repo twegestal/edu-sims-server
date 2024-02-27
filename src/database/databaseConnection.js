eb46138ed8bb8fcae8aea27849cc950d10953ac2
twegestal <96296150+twegestal@users.noreply.github.com>
2024-02-27 15:59:46 +0100

    Troubleshooting main
    Perhaps dotenv is not configed.

--- src/database/databaseConnection.js
+++ src/database/databaseConnection.js
@@ -1,4 +1,6 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connectToDatabase = async () => {
   const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
